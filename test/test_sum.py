eb923b606aa1f7be27ed1c35e8f38e576a050b51
TypicalCode0 <131657894+TypicalCode0@users.noreply.github.com>
2024-04-04 11:29:17 +0300

    Update test_sum.py

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,5 +1,17 @@
 import ../sum
 
 
-def test(a, b):
+def test_sum(a, b):
   assert sum(a,b)==a+b
+
+def test_minus(a, b):
+  assert minus(a,b)==a-b
+
+def test_mul(a, b):
+  assert mul(a,b) == a*b
+
+def test_pow(a, b):
+  assert pow(a, b) == a**b
+:
+def test_divide(a, b):
+  assert divide(a, b) == a / b
