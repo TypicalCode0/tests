899a4fd94f60b6bef5c2837dea7ea12a2b3c8545
TypicalCode0 <131657894+TypicalCode0@users.noreply.github.com>
2024-06-03 22:31:56 +0300

    Update test_sum.py

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,56 +1,16 @@
 from src.operations import * 
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'),
-    [
-        (10, 3, 13),
-        (-1, 5, 4),
-        (0, 0, 0),
-    ],
-)
-def test_sum(a, b, result):
-  assert sum(a,b)==result
+def test_sum():
+  assert sum(10,15) == 25
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'),
-    [
-        (10, 3, 7),
-        (-1, 5, -6),
-        (0, 0, 0),
-    ],
-)
-def test_minus(a, b, result):
-  assert minus(a,b)==result
+def test_minus():
+  assert minus(15,10) == 5
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'),
-    [
-        (10, 3, 30),
-        (-1, 5, -5),
-        (0, 0, 0),
-    ],
-)
-def test_mul(a, b, result):
-  assert mul(a,b) == result
+def test_mul():
+  assert mul(5,5) == 25
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'),
-    [
-        (10, 3, 1000),
-        (1, 0, 1),
-        (3, 2, 9),
-    ],
-)
-def test_pow(a, b, result):
-  assert pow(a, b) == result
+def test_pow():
+  assert pow(0, 10) == 0
 
-@pytest.mark.parametrize(
-    ('a', 'b', 'result'),
-    [
-        (6, 3, 2.0),
-        (-1, 5, -0.2),
-        (0, 5, 0.0),
-    ],
-)
-def test_divide(a, b, result):
-  assert divide(a, b) == result
+def test_divide():
+  assert divide(10, 2) == 5
