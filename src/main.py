250fa5eecd015a93597b9d6ee30865ba18db02fe
TypicalCode0 <131657894+TypicalCode0@users.noreply.github.com>
2024-06-04 22:46:35 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+"""asd"""
+from operations import sm, pw, mul, minus, divide
+
+
+while True:
+    print("Введите команду. Доступные команды:")
+    print("exit - выход из программы\nenter - ввод мат. выражения")
+    cmd = input()
+    if cmd == "exit":
+        break
+    elif cmd == "enter":
+        s = input("Введите выражение через пробел -> ").split()
+        print("Результат: ", sep="", end="")
+        if s[1] == "+":
+            print(sm(int(s[0]), int(s[2])))
+        elif s[1] == "-":
+            print(minus(int(s[0]), int(s[2])))
+        elif s[1] == "*":
+            print(mul(int(s[0]), int(s[2])))
+        elif s[1] == "/":
+            print(divide(int(s[0]), int(s[2])))
+        elif s[1] == "**":
+            print(pw(int(s[0]), int(s[2])))
