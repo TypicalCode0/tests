e8d176daa3925126726a9b77861e8f7d64fa542c
TypicalCode0 <131657894+TypicalCode0@users.noreply.github.com>
2024-06-04 22:43:57 +0300

    Update operations.py

--- src/operations.py
+++ src/operations.py
@@ -20,3 +20,24 @@ def pw(a: int, b: int) -> int:
 def divide(a: int, b: int) -> int:
     '''a / b'''
     return a // b
+
+
+while True:
+    print("Введите команду. Доступные команды:")
+    print("exit - выход из программы\nenter - ввод мат. выражения")
+    cmd = input()
+    if cmd == "exit":
+        break
+    elif cmd == "enter":
+        s = input("Введите выражение через пробел -> ").split()
+        print("Результат: ", sep="", end="")
+        if s[1] == "+":
+            print(sm(int(s[0]), int(s[2])))
+        elif s[1] == "-":
+            print(minus(int(s[0]), int(s[2])))
+        elif s[1] == "*":
+            print(mul(int(s[0]), int(s[2])))
+        elif s[1] == "/":
+            print(divide(int(s[0]), int(s[2])))
+        elif s[1] == "**":
+            print(pw(int(s[0]), int(s[2])))
